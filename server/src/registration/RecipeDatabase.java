53ee27462564c77aaa4f579d3104ef3cd5ec232a
dfs <davenliu13@gmail.com>
2018-10-20 19:21:43 -0400

    new

--- server/src/registration/RecipeDatabase.java
+++ server/src/registration/RecipeDatabase.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Iterator;
+
+import org.json.JSONObject;
 
 public class RecipeDatabase {
 	
@@ -14,6 +18,96 @@ public class RecipeDatabase {
 		con = DataBaseConnector.connect(con);
 	}
 	
+	// daven add start 10/20/18
+	
+	public String insertrecipe(JSONObject data) {
+		//List<String> name = new ArrayList<>(); 
+		//Recipe recipe = new Recipe();
+		//System.out.println(data);
+		try {	
+			String sql = "Select count(*) from recipe";
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			int recipeId = 1;
+			if(rs.next()) {
+				recipeId = rs.getInt(1) + 1;
+			}
+			//System.out.println(recipeId);
+			//System.out.println(data.getString("label") + " || " + data.getString("description")  + " || " +  data.getString("image") + " || " +  data.getString("URL") + " || " + data.getInt("servings") + " || " +  data.getDouble("calories") + " || " + data.getInt("totalTime"));
+			st.close();
+			sql = "insert into recipe (recipeId, nutrientsId, label, description, image, url, servings, "
+					+ "calories, totalTime) value (?,?,?,?,?,?,?,?,?)";
+			PreparedStatement st2 = con.prepareStatement(sql);
+			st2.setInt(1, recipeId);
+			st2.setInt(2, recipeId);
+			st2.setString(3, data.getString("label"));
+			st2.setString(4, data.getString("description"));
+			st2.setString(5, data.getString("image"));
+			st2.setString(6, data.getString("URL"));
+			st2.setInt(7, data.getInt("servings"));
+			st2.setDouble(8, data.getDouble("calories"));
+			st2.setInt(9, data.getInt("totalTime"));
+			st2.executeUpdate();
+			st2.close();
+			
+			insertNutrient(recipeId, data.getJSONObject("nutrient"));
+			insertIngredient(recipeId, data.getJSONObject("ingredient"));
+			return "Recipe insert success";
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		return "Recipe insert fail";
+	}
+	
+	//insert the nutrients
+		public void insertNutrient(int id, JSONObject data) {
+			try {
+				if(data.isEmpty())
+					return ;
+				//System.out.println(data.getDouble("fat") + " || " +data.getDouble("sugar"));
+				String sql = "insert into nutrient (nutrientsId, fat, sugar, protein, fiber, "
+						+ "sodium, cholesterol, carbs) value (?,?,?,?,?,?,?,?)";
+				PreparedStatement st = con.prepareStatement(sql);
+				st.setInt(1, id);
+				st.setDouble(2, data.getDouble("fat"));
+				st.setDouble(3, data.getDouble("sugar"));
+				st.setDouble(4, data.getDouble("protein"));
+				st.setDouble(5, data.getDouble("fiber"));
+				st.setDouble(6, data.getDouble("sodium"));
+				st.setDouble(7, data.getDouble("cholesterol"));
+				st.setDouble(8, data.getDouble("carbs"));
+				st.executeUpdate();
+				st.close();
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+		
+		//insert the ingredients
+		public void insertIngredient(int id, JSONObject data) {
+			
+			try {
+				if(data.isEmpty())
+					return ;
+				String sql = "insert into ingredient (recipeId, weight, ingredients) value (?,?,?)";
+				PreparedStatement st = con.prepareStatement(sql);
+				Iterator<String> keys = data.keys();
+				String key;
+				while(keys.hasNext()) {
+					key = keys.next();
+					st = con.prepareStatement(sql);
+					st.setInt(1, id);
+					st.setInt(2, data.getInt(key));
+					st.setString(3, key);
+					st.executeUpdate();
+				}
+				st.close();
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+		
+	// daven add end	10/20/18
 	//register,1. get add username from database table, 2.compare to user input, 3. register, or reject
 	public String insertrecipe(Recipe recipe) {
 		//List<String> name = new ArrayList<>(); 
