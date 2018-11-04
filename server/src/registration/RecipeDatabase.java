48ba1059b8940e01a56110a5172911bb64370ff1
AbsoluteKhaos <xabsolutekhaosx@gmail.com>
2018-11-04 11:50:42 -0500

    Added userHistory

--- server/src/registration/RecipeDatabase.java
+++ server/src/registration/RecipeDatabase.java
@@ -7,6 +7,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Iterator;
 
+import javax.json.*;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -135,10 +137,8 @@ public class RecipeDatabase {
 	}
 
 	//login 
-	public Recipe getRecipe(int recipeId) {
-		// TODO Auto-generated method stub
+	public JSONObject getRecipe(int recipeId) {
 		System.out.println(recipeId);
-		Recipe temp = new Recipe();
 		String sql = "select * from recipe where recipeId = ?";
 		try {
 			PreparedStatement st = con.prepareStatement(sql);
@@ -149,19 +149,78 @@ public class RecipeDatabase {
 				System.out.println(!rs.next());
 				return null;
 			}
-			System.out.println("1:"+rs.getInt(5) + " 2 :"+rs.getDouble(6)); 
-			temp.setRecipeId(rs.getInt(1));
-			temp.setnutrientsId(rs.getInt(2));
-			temp.setlabel(rs.getString(3));
-			temp.setdescription(rs.getString(4));
-			temp.setservings(rs.getInt(7));
-			temp.setcalories(rs.getDouble(8));
-			temp.settotalTime(rs.getInt(9));
+			System.out.println("lol"); 
+			JSONObject recipeInfo = new JSONObject();
+			recipeInfo.put("recipeId", recipeId);
+			recipeInfo.put("nutrientId", recipeId);
+			recipeInfo.put("label", rs.getString(3));
+			recipeInfo.put("description", rs.getString(4));
+			recipeInfo.put("image", rs.getString(5));
+			recipeInfo.put("URL",rs.getString(6));
+			recipeInfo.put("servings", rs.getInt(7));
+			recipeInfo.put("calories", rs.getDouble(8));
+			recipeInfo.put("totalTime", rs.getInt(9));
 			st.close();
-			return temp;
+
+			recipeInfo.put("nutrient", getNutrientInfo(recipeId));
+			recipeInfo.put("ingredient", getIngredientInfo(recipeId));
+		    return recipeInfo;
+
+		}catch(Exception e) {
+			System.out.println(e);
+		}		
+		return null;
+	}
+
+
+	public JSONObject getNutrientInfo(int recipeId) {
+		JSONObject nutrientInfo = new JSONObject();
+		String sql = "select * from nutrient where NutrientsId = ?";
+		try {
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, recipeId);
+			ResultSet rs = st.executeQuery();
+			System.out.println("Getting nutrient"); //xxxxx
+			if(!rs.next()) {
+				System.out.println(!rs.next());
+				return null;
+			}
+			nutrientInfo.put("nutrientId", rs.getInt(1));
+			nutrientInfo.put("fat", rs.getDouble(2));
+			nutrientInfo.put("sugar", rs.getDouble(3));
+			nutrientInfo.put("protein", rs.getDouble(4));
+			nutrientInfo.put("fiber", rs.getDouble(5));
+			nutrientInfo.put("sodium", rs.getDouble(6));
+			nutrientInfo.put("cholesterol", rs.getDouble(7));
+			nutrientInfo.put("carbs", rs.getDouble(8));
+			st.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		return nutrientInfo;
+	}
+
+	public JSONObject getIngredientInfo(int recipeId) {
+		String sql = "select * from ingredient where recipeId = ?";
+		try {
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, recipeId);
+			ResultSet rs = st.executeQuery();
+			System.out.println("Getting ingredient"); //xxxxx
+			//if(!rs.next()) {
+			//	System.out.println(!rs.next());
+			//	return null;
+			//}
+			JSONObject ingredientInfo = new JSONObject();
+			while (rs.next()) {
+				ingredientInfo.put( rs.getString(3), Integer.toString(rs.getInt(1) ));	
+			}
+			st.close();
+			return ingredientInfo;
 		}catch(Exception e) {
 			System.out.println(e);
 		}		
 		return null;
 	}
 }
+
--- server/src/registration/RecipeGetAPI.java
+++ server/src/registration/RecipeGetAPI.java
@@ -1,11 +1,14 @@
 package registration;
 
+import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.json.JSONObject;
 
 @Path("recipeget")
 public class RecipeGetAPI {
@@ -21,7 +24,7 @@ public class RecipeGetAPI {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Recipe getRecipe (Recipe data) {
+	public JSONObject getRecipe (Recipe data) {
 		System.out.print("start");
 		return dataBase.getRecipe(data.getrecipeId());
 	}
